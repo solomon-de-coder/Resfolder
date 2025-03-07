afe8bfa768fe92997415a499f78deba69cd8fbe6
solomon-de-coder <pcsolomon8@gmail.com>
2025-03-07 21:25:58 +0530

    commited in vs code

--- roughwork.py
+++ roughwork.py
@@ -1,2 +1,6 @@
-a = eval(input("enter the names:"))
-print(type(a))
+#first time trying to print the star pyramid pattern on own LOL
+a = int(input("enter the number of rows:"))
+for i in range (a+1,1,-1):
+    for j in range (i-1):
+        print("hi", end = ' ')
+    print()
